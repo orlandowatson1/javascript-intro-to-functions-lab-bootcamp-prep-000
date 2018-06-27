ffadc3fa9f270b995d2a2895b1e54c81018ad14e
Orlando Watson <orlando.watson@journalism.cuny.edu>
2018-06-27 17:26:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function logWhisper (string) {
   return string.toLowerCase()
 }
 function sayHiToGrandma (string) {
-  console.log('hello')
-  return string.toLowerCase()
+  console.log('I can\'t hear you')
+  return string.toUpperCase()
 }
