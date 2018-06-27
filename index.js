64b4052944c11ea64d454138efa0cc0671f557c1
Orlando Watson <orlando.watson@journalism.cuny.edu>
2018-06-27 18:18:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ function logWhisper (string) {
   return string.toLowerCase()
 }
 function sayHiToGrandma (string) {
-  if (string.toLowerCase() === string)
+  if (string.toLowerCase() === string) {
   return 'I can\'t hear you!';
   }
   if (string.toUpperCase() === string) {
   return 'YES INDEED!';
   }
-  if (string === 'I love you, Grandma.') {
+  if (string === 'I love u, Grandma.') {
   return 'I love you, too.';
   }
 }
